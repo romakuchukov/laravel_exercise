65dff5b034a80a5208c48f37db0811f1d020515a
Roma <roma.kuchukov.metarhythm@gmail.com>
2018-02-25 16:17:12 -0500

    split home template

---
+++ resources/views/pageparts/currency-select.blade.php
@@ -0,0 +1,17 @@
+<!--row -->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="white-box">
+            <div class="box-title">Select Currency Type
+                <div class="col-md-2 col-sm-4 col-xs-12 pull-right">
+                    <select class="form-control pull-right row b-none">
+                        <option vlaue="usd">USD</option>
+                        <option vlaue="eur">EUR</option>
+                        <option vlaue="aud">AUD</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.row -->
---
+++ resources/views/pageparts/percent-change.blade.php
@@ -0,0 +1,49 @@
+<!-- row -->
+<div class="row">
+    <!--col -->
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="white-box">
+            <div class="col-in row">
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <i data-icon="E" class="linea-icon linea-basic"></i>
+                    <h5 class="text-muted vb">HOUR</h5>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="counter text-right m-t-15 text-danger">{{$hour}}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.col -->
+    <!--col -->
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="white-box">
+            <div class="col-in row">
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <i class="linea-icon linea-basic" data-icon="&#xe01b;"></i>
+                    <h5 class="text-muted vb">DAY</h5>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="counter text-right m-t-15 text-megna">{{$day}}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.col -->
+    <!--col -->
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="white-box">
+            <div class="col-in row">
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <i class="linea-icon linea-basic" data-icon="&#xe00b;"></i>
+                    <h5 class="text-muted vb">WEEK</h5>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <h3 class="counter text-right m-t-15 text-primary">{{$week}}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.col -->
+</div>
+<!-- /.row -->
