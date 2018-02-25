0e30bda2cbc6efa4fd89c3b01f915bf4e5684841
Roma <roma.kuchukov.metarhythm@gmail.com>
2018-02-25 12:51:29 -0500

    split templates

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -4,77 +4,79 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <meta name="description" content="">
+    <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('imgs/favicon.png') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sidebar-nav.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/colors/blue-dark.css') }}" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
+    <div id="wrapper">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top m-b-0">
+            <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
+                <div class="top-left-part"><a class="logo" href="index.html"><b><img src="imgs/pixeladmin-logo.png" alt="home" /></b><span class="hidden-xs"><img src="imgs/pixeladmin-text.png" alt="home" /></span></a></div>
+                <ul class="nav navbar-top-links navbar-right pull-right">
+                    <li>
+                        <a class="profile-pic" href="#"> <img src="imgs/varun.jpg" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">Steave</b> </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-header -->
+            <!-- /.navbar-top-links -->
+            <!-- /.navbar-static-side -->
+        </nav>
+        <!-- Left navbar-header -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse slimscrollsidebar">
+                <ul class="nav" id="side-menu">
+                    <li style="padding: 10px 0 0;">
+                        <a href="index.html" class="waves-effect"><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i><span class="hide-menu">Dashboard</span></a>
+                    </li>
+                    <li>
+                        <a href="profile.html" class="waves-effect"><i class="fa fa-user fa-fw" aria-hidden="true"></i><span class="hide-menu">Profile</span></a>
+                    </li>
+                    <li>
+                        <a href="basic-table.html" class="waves-effect"><i class="fa fa-table fa-fw" aria-hidden="true"></i><span class="hide-menu">Basic Table</span></a>
+                    </li>
+                    <li>
+                        <a href="fontawesome.html" class="waves-effect"><i class="fa fa-font fa-fw" aria-hidden="true"></i><span class="hide-menu">Icons</span></a>
+                    </li>
+                    <li>
+                        <a href="map-google.html" class="waves-effect"><i class="fa fa-globe fa-fw" aria-hidden="true"></i><span class="hide-menu">Google Map</span></a>
+                    </li>
+                    <li>
+                        <a href="blank.html" class="waves-effect"><i class="fa fa-columns fa-fw" aria-hidden="true"></i><span class="hide-menu">Blank Page</span></a>
+                    </li>
+                    <li>
+                        <a href="404.html" class="waves-effect"><i class="fa fa-info-circle fa-fw" aria-hidden="true"></i><span class="hide-menu">Error 404</span></a>
+                    </li>
+                </ul>
+                <div class="center p-20">
+                    <span class="hide-menu"><a href="http://wrappixel.com/templates/pixeladmin/" target="_blank" class="btn btn-danger btn-block btn-rounded waves-effect waves-light">Upgrade to Pro</a></span>
                 </div>
             </div>
-        </nav>
-
+        </div>
+        <!-- Left navbar-header end -->
+        <!-- Page Content -->
         @yield('content')
+        <!-- /#page-wrapper -->
     </div>
-
-    <!-- Scripts -->
+    <!-- /#wrapper -->
+    <!-- jQuery -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,166 @@
 @extends('layouts.wrapper')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row bg-title">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h4 class="page-title">Dashboard</h4> </div>
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12"> <a href="http://wrappixel.com/templates/pixeladmin/" target="_blank" class="btn btn-danger pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Upgrade to Pro</a>
+                <ol class="breadcrumb">
+                    <li><a href="#">Dashboard</a></li>
+                </ol>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- row -->
+        <div class="row">
+            <!--col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="white-box">
+                    <div class="col-in row">
+                        <div class="col-md-6 col-sm-6 col-xs-6"> <i data-icon="E" class="linea-icon linea-basic"></i>
+                            <h5 class="text-muted vb">MYNEW CLIENTS</h5> </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6">
+                            <h3 class="counter text-right m-t-15 text-danger">23</h3> </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%"> <span class="sr-only">40% Complete (success)</span> </div>
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <!--col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="white-box">
+                    <div class="col-in row">
+                        <div class="col-md-6 col-sm-6 col-xs-6"> <i class="linea-icon linea-basic" data-icon="&#xe01b;"></i>
+                            <h5 class="text-muted vb">NEW PROJECTS</h5> </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6">
+                            <h3 class="counter text-right m-t-15 text-megna">169</h3> </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-megna" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%"> <span class="sr-only">40% Complete (success)</span> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <!--col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="white-box">
+                    <div class="col-in row">
+                        <div class="col-md-6 col-sm-6 col-xs-6"> <i class="linea-icon linea-basic" data-icon="&#xe00b;"></i>
+                            <h5 class="text-muted vb">NEW INVOICES</h5> </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6">
+                            <h3 class="counter text-right m-t-15 text-primary">157</h3> </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%"> <span class="sr-only">40% Complete (success)</span> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <!--row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="white-box">
+                    <h3 class="box-title">Sales Difference</h3>
+                    <ul class="list-inline text-right">
+                        <li>
+                            <h5><i class="fa fa-circle m-r-5" style="color: #dadada;"></i>Site A View</h5>
+                        </li>
+                        <li>
+                            <h5><i class="fa fa-circle m-r-5" style="color: #aec9cb;"></i>Site B View</h5>
+                        </li>
+                    </ul>
+                    <div id="morris-area-chart2" style="height: 370px;"></div>
                 </div>
             </div>
         </div>
+        <!--row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="white-box">
+                    <div class="box-title">Recent sales
+                        <div class="col-md-2 col-sm-4 col-xs-12 pull-right">
+                            <select class="form-control pull-right row b-none">
+                                <option>March 2016</option>
+                                <option>April 2016</option>
+                                <option>May 2016</option>
+                                <option>June 2016</option>
+                                <option>July 2016</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table ">
+                            <thead>
+                                <tr>
+                                    <th>NAME</th>
+                                    <th>STATUS</th>
+                                    <th>DATE</th>
+                                    <th>PRICE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="txt-oflo">Pixel admin</td>
+                                    <td>SALE</td>
+                                    <td class="txt-oflo">April 18</td>
+                                    <td><span class="text-success">$24</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Real Homes</td>
+                                    <td>EXTENDED</td>
+                                    <td class="txt-oflo">April 19</td>
+                                    <td><span class="text-info">$1250</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Medical Pro</td>
+                                    <td>TAX</td>
+                                    <td class="txt-oflo">April 20</td>
+                                    <td><span class="text-danger">-$24</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Hosting press</td>
+                                    <td>SALE</td>
+                                    <td class="txt-oflo">April 21</td>
+                                    <td><span class="text-success">$24</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Helping Hands</td>
+                                    <td>MEMBER</td>
+                                    <td class="txt-oflo">April 22</td>
+                                    <td><span class="text-success">$24</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Digital Agency</td>
+                                    <td>SALE</td>
+                                    <td class="txt-oflo">April 23</td>
+                                    <td><span class="text-danger">-$14</span></td>
+                                </tr>
+                                <tr>
+                                    <td class="txt-oflo">Helping Hands</td>
+                                    <td>MEMBER</td>
+                                    <td class="txt-oflo">April 22</td>
+                                    <td><span class="text-success">$64</span></td>
+                                </tr>
+                            </tbody>
+                        </table> <a href="#">Check all the sales</a> </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
     </div>
+    <!-- /.container-fluid -->
+    <footer class="footer text-center"> 2017 &copy; Pixel Admin brought to you by wrappixel.com </footer>
 </div>
 @endsection
