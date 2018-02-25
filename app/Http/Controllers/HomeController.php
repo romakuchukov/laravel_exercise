874a4faabbb17007eb073afc8494140d31cb0162
Roma <roma.kuchukov.metarhythm@gmail.com>
2018-02-25 15:50:13 -0500

    simplified currency array

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,19 +15,49 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Gets bit coin in US and other currencies
+     *
+     * @return specific currency bitcoin array
+     */
+    private function get_currency_json ($currency) {
+        return json_decode(file_get_contents("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=$currency"), true);
+    }
+
+    /**
+     * merges several currency arrays into one
+     *
+     * @return one currency array
+     */
+
+    private function get_merged_currencies($currency_types) {
+        $currency_stack = [];
+
+        foreach($currency_types as $type):
+            $currency_stack = array_unique(array_merge($currency_stack, $this->get_currency_json($type)[0]), SORT_REGULAR);
+        endforeach;
+
+        return $currency_stack;
+    }
+
+    /**
+     * Show the application dashboard and populate it with data.
      *
      * @return view
      */
     public function index()
     {
-        $bitcoin = json_decode(file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin'), true)[0];
+        $bitcoin = $this->get_merged_currencies(['EUR', 'AUD']);
 
         $data = [
+            'price_usd' => $bitcoin['price_usd'],
+            'price_eur' => $bitcoin['price_eur'],
+            'price_aud' => $bitcoin['price_aud'],
+
             'hour' => $bitcoin['percent_change_1h'],
             'day'  => $bitcoin['percent_change_24h'],
             'week' => $bitcoin['percent_change_7d'],
         ];
+
         return view('home')->with($data);
     }
 }
