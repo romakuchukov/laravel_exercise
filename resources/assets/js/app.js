67391644cd9b453cb9cdf26381e016d4c6f82e5e
Roma <roma.kuchukov.metarhythm@gmail.com>
2018-02-26 12:49:09 -0500

    put some old code back

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,72 +20,53 @@
 // const app = new Vue({
 //     el: '#app'
 // });
+
 function precisionRound(number, precision) {
   var factor = Math.pow(10, precision);
   return Math.round(number * factor) / factor;
 }
 
 jQuery(function($) {
-    if (localStorage.getItem('logoutPercent') === null) {
-        localStorage.setItem('logoutPercent', JSON.stringify(0));
-    }
 
-    var $hour_icon = $('#hour');
-    var $day_icon  = $('#day');
-    var $week_icon = $('#week');
-
-    (hour < 0) ? $day_icon.addClass('negative') : $.noop();
-    (day < 0) ? $hour_icon.addClass('negative') : $.noop();
-    (week < 0) ? $week_icon.addClass('negative'): $.noop();
-
-    //var $currencySelect = $('#currency-select');
-
-    $('#currency-select').change(function() {
-
-        // var currency = $(this).val().split('|')[0];
-        var price = $(this).val().split('|')[1];
-        var selectedCurrency = $(this).children('option').filter(':selected').text();
-
-        $('#current-currency').text(selectedCurrency);
-        $('#current-price').html('')
-        $('#current-price').html('<i class="fa fa-money" id="money-icon" aria-hidden="true"></i>' + price);
-
-        //localStorage.setItem('previousPriceCurr', JSON.stringify([currency, price]));
-
-    });
-
-    //var storedData = JSON.parse(localStorage.getItem('previousPriceCurr'));
-
-
-    // $currencySelect.find('option').filter(function() {
-
-    //     // var newVal = $(this).val().slice(4);
-    //     // var oldVal = storedData[1];
-    //     // var percentChange = (newVal-oldVal)/oldVal;
-
-    //     // //if (percentChange === Infinity) { percentChange = 0; }
-
-    //     // $(this).data('percent-change', precisionRound(percentChange, 2));
-
-    //     //return (storedData[0] === $(this).val().slice(0, 3));
-
-    // }).filter(function() {
-
-
-
-    // }).prop('selected', true).trigger('change');
-
-    //$currencySelect = $('#currency-select');
-    var oldPrice = localStorage.getItem('logoutPercent');
+    // calculate percentage since last logout
+    var $percentChange = $('#percent-change');
+    var oldPrice = localStorage.getItem('logoutPercent') || price_usd;
     var newPrice = price_usd;
     var percentChangeNum = precisionRound((newPrice-oldPrice)/oldPrice, 2);
 
-    console.log(localStorage.getItem('logoutPercent'));
-
-    var $percentChange = $('#percent-change');
-    $percentChange.html($percentChange.html() + ((percentChangeNum === Infinity) ? 0 : percentChangeNum) + '%');
+    $percentChange.html($percentChange.html() + (percentChangeNum) + '%');
 
     (percentChangeNum < 0) ? $percentChange.find('i').addClass('negative') : $.noop();
+    (hour < 0) ? $('#hour').addClass('negative') : $.noop();
+    (day < 0) ? $('#day').addClass('negative') : $.noop();
+    (week < 0) ? $('#week').addClass('negative'): $.noop();
+
+
+    if (localStorage.getItem('previousPriceCurr') === null) {
+        localStorage.setItem('previousPriceCurr', JSON.stringify(['usd', price_usd]));
+    }
+    var $currencySelect = $('#currency-select');
+    var storedData = JSON.parse(localStorage.getItem('previousPriceCurr'));
+    //select currencies and store it
+    $currencySelect.change(function() {
+
+        var $currentCurrency = $('#current-currency');
+        var $currentPrice = $('#current-price');
+        var price = $(this).val().split('|')[1];
+        var currency = $(this).val().split('|')[0];
+
+        $currentCurrency.text($(this).children('option').filter(":selected").text());
+
+        $currentPrice.html('')
+        $currentPrice.html('<i class="fa fa-money" id="money-icon" aria-hidden="true"></i>' + price);
+
+        localStorage.setItem('previousPriceCurr', JSON.stringify([currency, price]));
+    });
+
+    // select
+    $currencySelect.find('option').filter(function() {
+        return (storedData[0] === $(this).val().slice(0, 3));
+    }).prop('selected', true).trigger('change');
 
     $('#logout-form').find('input').click(function(e) {
         localStorage.setItem('logoutPercent', JSON.stringify(price_usd));
